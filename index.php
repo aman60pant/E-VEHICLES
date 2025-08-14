5add34af495651c7e16a76e3e91f140025066054
aman60pant <aman60pant@gmail.com>
2025-08-14 13:55:15 +0530

    modified index.php in .

--- index.php
+++ index.php
@@ -148,7 +148,7 @@
     ?>
     <!-- Navbar End -->
 
-    
+
 
     <!-- Carousel Start -->
     <div class="container-fluid p-0 mb-5">
@@ -206,23 +206,6 @@
                         </div>
                     </div>
                 </div>
-                <!-- <div class="carousel-item">
-                    <img class="w-100" src="img/banner8.jpg" alt="Image">
-                    <div class="carousel-caption d-flex align-items-center">
-                        <div class="container">
-                            <div class="row align-items-center justify-content-center justify-content-lg-start">
-                                <div class="col-10 col-lg-7 text-center text-lg-start">
-                                    <h6 class="text-white text-uppercase mb-3 animated slideInDown">// EV-Atul Auto //</h6>
-                                    <h4 class="display-3 text-white mb-4 pb-3 animated slideInDown">Eco-Friendly Rides: Discover the Future EV-Auto</h4>
-                                    <a href="" class="btn btn-primary py-3 px-5 animated slideInDown">Learn More<i class="fa fa-arrow-right ms-3"></i></a>
-                                </div>
-                                <div class="col-lg-5 d-none d-lg-flex animated zoomIn">
-                                    <img class="img-fluid" src="img/carousel2.png" alt="">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#header-carousel" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
